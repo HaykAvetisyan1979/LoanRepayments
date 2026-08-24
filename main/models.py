969dbf3023d860c780023928208f13b33a75fd90
Hayk Avetisyan <haykave@yahoo.com>
2026-08-24 19:15:00 +0400

    24.08.26+11

--- main/models.py
+++ main/models.py
@@ -86,9 +86,9 @@ class Calculation:
     def WeightedAverageRate(self):
         ds = DataToPandasDataset(table="PastDueLoanRazm")
         df = ds.load_loans_dataframe()
-        # for r in df.rows:
-        #     if df['Balance'][r] is 'NULL':
-        #         df['Balance'][r] = 0
+        for r in df.rows:
+            if df['Balance'][r] is 'NULL':
+                df['Balance'][r] = 0
 
         result = math.sumprod(df['AgreementPercent'],df['Balance'])/sum(df['Balance'])
         return result
